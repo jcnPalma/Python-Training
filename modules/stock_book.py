e9a2e99d5dc82445506c7aae6e600c72ecf85e4c
jcnPalma <c_jnpalma@unilab.com.ph>
2025-01-03 10:36:18 +0800

    Created a way to update pub date and isbn

--- modules/stock_book.py
+++ modules/stock_book.py
@@ -1,8 +1,10 @@
 class Stockbook:
+    # Sets the title and author of the book on creation
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
+    # Converts the book object to a dictionary
     def get_book(self):
         booked = {
             "title": self.title,
